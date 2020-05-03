0276c28fda6aa3dface27147a9ff076c68681f8c
MMDBadCoder <gitmmdgit@gmail.com>
2020-05-03 18:15:41 +0430

    add gyroscop full example

---
+++ app/src/main/java/com/mmd/kasra/GyroLabActivity.java
@@ -0,0 +1,13 @@
+package com.mmd.kasra;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GyroLabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gyro_lab);
+    }
+}
